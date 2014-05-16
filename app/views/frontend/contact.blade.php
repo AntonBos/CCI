3dc19ff7f08476da55c9303877b4cfbe60bcebb1
Anton Boshoff <anton@digitalpro.co.za>
2014-05-16 16:13:22 +0200

    added contact details table

--- app/views/frontend/contact.blade.php
+++ app/views/frontend/contact.blade.php
@@ -13,7 +13,93 @@
                             </div>
                             <div class="subCatCont">
                             	<h1>Contact</h1>
-                            	<p>Pellentesque nibh felis, eleifend id, commodo in, interdum vitae, leo. Praesent eu elit. Ut eu ligula. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Maecenas elementum augue nec nisl. Proin auctor lorem at nibh. Curabitur nulla purus, feugiat id, elementum in, lobortis quis, pede. Vivamus sodales adipiscing sapien. Vestibulum posuere nulla eget wisi. Integer volutpat ligula eget enim. Suspendisse vitae arcu. Quisque pellentesque. Nullam consequat, sem vitae rhoncus tristique, mauris nulla fermentum est, bibendum ullamcorper sapien magna et quam. Sed dapibus vehicula odio. Proin bibendum gravida nisl. Fusce lorem. Phasellus sagittis, nulla in hendrerit laoreet, libero lacus feugiat urna, eget hendrerit pede magna vitae lorem. Praesent mauris.</p>
+                            	<table>
+                                    <thead>
+                                        <tr>
+                                            <th><p>Location</p></th>
+                                            <th><p>Cape Town (Head Office)</p></th>
+                                            <th><p>Gauteng</p></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <p>Postal Address</p>
+                                            </td>
+                                            <td>
+                                                <p>PO Box 434<br>
+                                                Howard Place<br>
+                                                7450<br>
+                                                South Africa</p>
+                                            </td>
+                                            <td>
+                                                <p>PO Box 1547<br>
+                                                Rant-en-Dal<br>
+                                                1751<br>
+                                                South Africa</p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <p>Physical Address</p>
+                                            </td>
+                                            <td>
+                                                <p>Unit A30<br>
+                                                Pinelands Business Park<br>
+                                                Avonduur Road<br>
+                                                Pinelands<br>
+                                                7405<br>
+                                                Cape Town<br>
+                                                South Africa</p>
+                                            </td>
+                                            <td>
+                                                <p>Unit A Krynaiuw Office Park<br>
+                                                14 Ontdekkers Road<br>
+                                                Roodepoort<br>
+                                                Gauteng<br>
+                                                South Africa</p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <p>Telephone</p>
+                                            </td>
+                                            <td>
+                                                <p>+27 (21) 531 0850</p>
+                                            </td>
+                                            <td>
+                                                <p>+27 (11) 664 6055</p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <p>Fax</p>
+                                            </td>
+                                            <td>
+                                                <p>+27 (21) 531 0851</p>
+                                            </td>
+                                            <td>
+                                                <p>+27 (11) 664 6194</p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <p>Email</p>
+                                            </td>
+                                            <td colspan="2">
+                                                <p style="text-align: center;"><a href="mailto:contact@cci.co.za" target="_blank">contact@cci.co.za</a></p>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <p>Website</p>
+                                            </td>
+                                            <td colspan="2">
+                                                <p style="text-align: center;"><a href="http://cci.co.za/" target="_blank">www.cci.co.za/</a></p>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
 
