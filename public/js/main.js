4f7f66c6ab03c589b0793852db5052ab9244678b
Anton Boshoff <anton@digitalpro.co.za>
2014-06-18 12:10:41 +0200

    updated main.js ( navigation toggle )

--- public/js/main.js
+++ public/js/main.js
@@ -227,7 +227,7 @@ cci.go = {
 	navShowHide: function() {
 		var navUl = $('nav ul');
 
-		if ( window.innerWidth > 768 || $("html").hasClass("ie8") ) {
+		if ( window.innerWidth > 767 || $("html").hasClass("ie8") ) {
 			navUl.show();
 		} else {
 			navUl.hide();
