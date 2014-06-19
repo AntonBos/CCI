a32903dffae9be8367ec2ebc6b14145da39961d0
Anton Boshoff <anton@digitalpro.co.za>
2014-06-19 11:13:30 +0200

    added test blade for juniper code

---
+++ app/views/frontend/test.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.frontend')
+@section('content')
+						<div class="contentHead">
+                            <h1>Contact</h1>
+                            <ul><li><a href="#">&nbsp;</a></li></ul>
+                        </div>
+
+                        <div class="contentBody">
+
+                        
+                        </div>
+
+@endsection
